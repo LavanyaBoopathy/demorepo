5065b6b4fde130204ba68f098097034eff9e36b6
lavanyaboopathy <blavanyaboopathy2gmail.com>
2020-04-24 15:19:30 +0530

    third push by worspace person

--- src/main/java/com/qa/libs/baseclass.java
+++ src/main/java/com/qa/libs/baseclass.java
@@ -4,6 +4,6 @@ public class baseclass {
 
 	public void test()
 	{
-		System.out.println("updated by workspace1 person on cloned repo");
+		System.out.println(" newly updated by workspace1 person on cloned repo");
 	}
 }
